ad262bb8d92b74d79b9f34a83b0827cf33ae72e8
Pavel <43359673+Warasar@users.noreply.github.com>
2020-05-19 17:03:36 +0900

    Add files via upload

---
+++ из5.1/5.1.cpp
@@ -0,0 +1,24 @@
+﻿/*
+Выведите по одному в строке все трех значные простые числа
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, k;
+    for (i = 100; i <= 999; i++) 
+    {
+        k = 0;
+        for (j = 1; j <= i; j++) 
+        {
+            n = i%j;
+            if (n == 0)
+                k++;
+        }
+        if (k == 2)
+            cout << i << " ";
+    }
+}
